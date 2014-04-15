0125a6bba855a732ff7fc8c0cf1fd94eec74ef0e
shihang <study1228.sh@gmail.com>
2014-04-15 18:16:44 +0800

    在LoginServlet中添加forward方法，并且转向register.jsp页面

--- src/main/java/com/order/LoginServlet.java
+++ src/main/java/com/order/LoginServlet.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
+import javax.servlet.RequestDispatcher;
 import java.io.IOException;
 
 public class LoginServlet extends HttpServlet
@@ -12,6 +13,12 @@ public class LoginServlet extends HttpServlet
     {
         resp.setCharacterEncoding("UTF-8");
         resp.setContentType("text/html;charset=UTF-8");
-        resp.getWriter().println("这是我的第一个毕业设计");
+        forward("register",req,resp);
+        
+    }
+    public void forward(String page, HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException
+    {
+        String jsp = "/WEB-INF/" + page + ".jsp";
+        getServletContext().getRequestDispatcher(jsp).forward(req,resp);
     }
 }
