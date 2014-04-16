c6c374484ae35416bc084291837b085a23dc8c48
shihang <study1228.sh@gmail.com>
2014-04-16 11:41:39 +0800

    添加doPost()方法，用于实现新增user的功能

--- src/main/java/com/order/LoginServlet.java
+++ src/main/java/com/order/LoginServlet.java
@@ -14,9 +14,43 @@ public class LoginServlet extends HttpServlet
         forward("register",req,resp);
         
     }
+    
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException
+    {
+        String action = req.getParameter("action");
+        if ("register".equals(action))
+        {
+        String userName = req.getParameter("user_name");
+        String userPassword = req.getParameter("password");
+        String repeatPassword = req.getParameter("repeat_password");
+        String userTrueName = req.getParameter("true_name");
+        String userAddress = req.getParameter("address");
+        String userSex = req.getParameter("sex");
+        String userPhoneNumber = req.getParameter("phone_number");
+        String userMail = req.getParameter("mail");
+     
+        UserVo user = new UserVo();
+        user.setUserName(userName);
+        user.setUserPassword(userPassword);
+        user.setRepeatPassword(repeatPassword);
+        user.setUserTrueName(userTrueName);
+        user.setUserAddress(userAddress);
+        user.setUserSex(userSex);
+        user.setUserPhoneNumber(userPhoneNumber);
+        user.setUserMail(userMail);
+        
+        UserDao userDao = new UserDao();
+        userDao.insertUser(user);
+        
+        
+       // forward("/userlogin.jsp", req, resp);
+        }
+    }
+    
     public void forward(String page, HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException
     {
         String jsp = "/WEB-INF/" + page + ".jsp";
         getServletContext().getRequestDispatcher(jsp).forward(req,resp);
     }
 }
+ 
