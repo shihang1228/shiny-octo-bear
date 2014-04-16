c893028183cb14b568a312b6d5a542b24f56d540
shihang <study1228.sh@gmail.com>
2014-04-16 11:37:31 +0800

    新增DbManage.java，用于和数据库建立连接和关闭连接

---
+++ src/main/java/com/order/DbManage.java
@@ -0,0 +1,74 @@
+package com.order;
+
+import java.sql.SQLException;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class DbManage
+{
+    static final String jdbcUrl = "jdbc:mysql://localhost/test?" + "user=root" + "&password=";
+    static final String jdbcDriver = "com.mysql.jdbc.Driver";
+    
+    public Connection conn = null;
+    private ResultSet rs = null;
+    private PreparedStatement pstmt = null;
+    private DbManage()
+    {
+        
+    }
+    public static DbManage newInstance() 
+    {
+        DbManage dbManage = new DbManage();
+        try
+        {
+            dbManage.conn = dbManage.createConnection();
+        }
+        catch(SQLException ex)
+        {
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+            System.out.println("error");
+        }
+        return dbManage;
+        
+    }
+    protected void close(AutoCloseable obj) 
+    {
+        try
+        {
+            obj.close();
+        }
+        catch(Exception ex)
+        {
+            //ignore;
+        }        
+    }
+    
+    public void close()
+    {
+        close(rs);
+        rs = null;
+        
+        close(conn);
+        conn = null;
+        
+        close(pstmt);
+        pstmt = null;
+
+    }
+    protected Connection createConnection() throws SQLException
+    {
+        try
+        {        
+            Class.forName(jdbcDriver).newInstance();    
+        }
+        catch(Exception ex)
+        {
+            //ignore
+        }
+        return DriverManager.getConnection(jdbcUrl);
+    }
+}
