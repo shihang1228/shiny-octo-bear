909e3261f8ecfad0cebd5e9d2aa16fd4ca8240db
shihang <study1228.sh@gmail.com>
2014-04-20 12:51:27 +0800

    添加后台登录的servlet，并且实现登录功能

---
+++ src/main/java/com/order/BackLoginServlet.java
@@ -0,0 +1,33 @@
+package com.order;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.servlet.ServletException;
+import javax.servlet.RequestDispatcher;
+import java.io.IOException;
+
+public class BackLoginServlet extends HttpServlet
+{
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)throws ServletException,IOException
+    {
+        forward("backLogin",req,resp);
+    }
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException,IOException
+    {
+        String userName = req.getParameter("back_username");
+        String password = req.getParameter("back_password");
+        if(userName.equals("admin") && password.equals("abc"))
+        {
+            HttpSession session = req.getSession();
+            session.setAttribute("memberId",0L);
+            forward("backLoginSuccess",req,resp);
+        }
+    }
+    public void forward(String page,HttpServletRequest req,HttpServletResponse resp)throws ServletException,IOException
+    {
+        String jsp = "/WEB-INF/" + page + ".jsp";
+        getServletContext().getRequestDispatcher(jsp).forward(req,resp);
+    }
+}
