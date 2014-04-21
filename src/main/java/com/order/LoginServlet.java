a90acca5d6d34c175d49fd82d806d6a891ff3b01
shihang <study1228.sh@gmail.com>
2014-04-21 10:49:25 +0800

    fixbug:将forward方法中的多余空格删除

--- src/main/java/com/order/LoginServlet.java
+++ src/main/java/com/order/LoginServlet.java
@@ -56,7 +56,7 @@ public class LoginServlet extends HttpServlet
                 {
                     HttpSession session = req.getSession();
                     session.setAttribute("memberId",0L);
-                    forward("loginSuccess ",req,resp);                  
+                    forward("loginSuccess",req,resp);                  
                 }
                 else
                 {
